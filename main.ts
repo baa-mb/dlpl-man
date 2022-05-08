4165f74e16508eee5acca06ff4ed6a4f567520e0
baa <34665120+baa-mb@users.noreply.github.com>
2022-05-08 16:21:15 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,33 @@
+function spiele (anz: number) {
+    for (let Index = 0; Index <= anz; Index++) {
+        anzeige()
+    }
+    basic.showString("A")
+}
+input.onSound(DetectedSound.Loud, function () {
+    spiele(10)
+})
+input.onButtonPressed(Button.A, function () {
+    spiele(10)
+})
+input.onGesture(Gesture.Shake, function () {
+    spiele(1)
+})
+function anzeige () {
+    basic.clearScreen()
+    basic.showString("HELLO")
+    for (let index = 0; index < 3; index++) {
+        basic.showIcon(IconNames.SmallHeart)
+        basic.showIcon(IconNames.Heart)
+    }
+    basic.clearScreen()
+    music.startMelody(music.builtInMelody(Melodies.Chase), MelodyOptions.OnceInBackground)
+    basic.showString("AUSTRO-TEC")
+    for (let index = 0; index < 3; index++) {
+        basic.showIcon(IconNames.SmallHeart)
+        basic.showIcon(IconNames.Heart)
+    }
+    basic.pause(1000)
+}
+basic.showIcon(IconNames.Heart)
+basic.showString("A")
